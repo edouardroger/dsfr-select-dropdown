40c0f18fff7f21e91db49bfff6c91eebabcb982c
Édouard Roger <edouard.roger+github@gmail.com>
2024-10-29 20:57:47 +0100

    Corrections et ajout de paramètres.

--- src/DsfrSelectDropdown.vue
+++ src/DsfrSelectDropdown.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <p class="fr-label fr-mb-1w" aria-hidden="true">{{ label }}<span class="fr-hint-text" v-show="hint">{{ hint
+        }}</span></p>
     <ul class="fr-tags-group" aria-label="Filtres appliqués">
       <li v-for="(tag, index) in selectedOptions" :key="tag">
         <button type="button" class="fr-tag fr-tag--sm fr-tag--dismiss" @click="removeTag(tag)"
@@ -8,16 +10,18 @@
         </button>
       </li>
     </ul>
-    <div class="fr-input-group__relative">
-      <button @click="toggleDropdown" class="fr-select" :aria-expanded="isDropdownOpen" :aria-controls="dropdownId">
+    <div class="fr-input-group__relative" :aria-labelledby="dropdownId + '-label'">
+      <button @click="toggleDropdown" class="fr-select text-left" :aria-expanded="isDropdownOpen"
+        :aria-controls="dropdownId" :aria-label="label + hint + selectedOptionsLabel">
         {{ selectedOptionsLabel }}
       </button>
       <div v-if="isDropdownOpen" :id="dropdownId">
         <div class="fr-collapse fr-dropdown-shadow" :class="{ 'fr-collapse--expanded': isDropdownOpen }">
-          <div class="fr-menu__alt fr-my-2w">
-            <div class="fr-toggle fr-mb-2w">
-              <input type="checkbox" class="fr-toggle__input" id="toggle-selection" @click="selectAll">
-              <label class="fr-toggle__label" for="toggle-selection">Sélectionner toutes les options</label>
+          <div class="fr-menu__alt fr-mb-2w">
+            <div class="fr-btns-group" v-show="showSelectAllBtn">
+              <button class="fr-btn fr-btn--secondary fr-btn--sm fr-mb-2w" @click="selectAll">
+                Tout {{ allSelected ? "désélectionner" : "sélectionner" }}
+              </button>
             </div>
             <div class="fr-input-group">
               <input type="search" aria-label="Filtre des options affichées" title="Filtre des options affichées"
@@ -25,9 +29,11 @@
             </div>
             <p :id="'search-options-' + dropdownId" role="status" aria-atomic="true" class="fr-sr-only">
               {{ filteredOptions.length }} options affichées</p>
-
             <fieldset class="fr-fieldset">
-              <legend class="fr-fieldset__legend">{{ fieldsetLegend }}</legend>
+              <legend class="fr-fieldset__legend fr-fieldset__legend--regular">{{ fieldsetLegend }}<span
+                  class="fr-hint-text" v-show="fieldsetLegendHint">{{ fieldsetLegendHint
+                  }}</span>
+              </legend>
               <div class="fr-fieldset__element" v-for="(option, index) in filteredOptions" :key="option.value">
                 <div class="fr-checkbox-group">
                   <input type="checkbox" :id="option.value" :value="option.value" v-model="selectedOptions">
@@ -49,9 +55,22 @@ export default {
       type: Array,
       required: true
     },
+    label: {
+      type: String,
+      required: true
+    },
+    hint: {
+      type: String
+    },
     fieldsetLegend: {
       type: String,
       default: 'Choix de l\'option'
+    },
+    fieldsetLegendHint: {
+      type: String
+    },
+    showSelectAllBtn: {
+      type: Boolean
     }
   },
   data() {
@@ -114,9 +133,14 @@ export default {
 .fr-menu__alt {
   padding: 1rem;
   min-height: 300px;
+  overflow-y: auto;
 }
 
 .fr-dropdown-shadow {
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.text-left {
+  text-align: left;
 }
 </style>
