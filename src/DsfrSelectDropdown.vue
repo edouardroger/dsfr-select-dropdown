5f54801aa71f3669f683b0b1d49d002779ffcac9
Édouard Roger <edouard.roger+github@gmail.com>
2024-10-31 12:08:18 +0100

    Gestion de la touche Échap et modification de la syntaxe du composant.

--- src/DsfrSelectDropdown.vue
+++ src/DsfrSelectDropdown.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <p class="fr-label fr-mb-1w" aria-hidden="true">{{ label }}<span class="fr-hint-text" v-show="hint">{{ hint
-        }}</span></p>
+    <p class="fr-label fr-mb-1w" aria-hidden="true">{{ label }}
+      <span class="fr-hint-text" v-show="hint">{{ hint }}</span>
+    </p>
     <ul class="fr-tags-group" aria-label="Filtres appliqués">
-      <li v-for="(tag, index) in selectedOptions" :key="tag">
+      <li v-for="tag in selectedOptions" :key="tag">
         <button type="button" class="fr-tag fr-tag--sm fr-tag--dismiss" @click="removeTag(tag)"
           :aria-label="'Supprimer le filtre ' + getOptionLabel(tag)">
           {{ getOptionLabel(tag) }}
@@ -12,7 +13,7 @@
     </ul>
     <div class="fr-input-group__relative" :aria-labelledby="dropdownId + '-label'">
       <button @click="toggleDropdown" class="fr-select text-left" :aria-expanded="isDropdownOpen"
-        :aria-controls="dropdownId" :aria-label="label + hint + selectedOptionsLabel">
+        :aria-controls="dropdownId" :aria-label="label + ' ' + hint + ' ' + selectedOptionsLabel" ref="selectButtonRef">
         {{ selectedOptionsLabel }}
       </button>
       <div v-if="isDropdownOpen" :id="dropdownId">
@@ -28,16 +29,16 @@
                 v-model="query" placeholder="Rechercher une option..." class="fr-input" />
             </div>
             <p :id="'search-options-' + dropdownId" role="status" aria-atomic="true" class="fr-sr-only">
-              {{ filteredOptions.length }} options affichées</p>
+              {{ filteredOptions.length }} options affichées
+            </p>
             <fieldset class="fr-fieldset">
-              <legend class="fr-fieldset__legend fr-fieldset__legend--regular">{{ fieldsetLegend }}<span
-                  class="fr-hint-text" v-show="fieldsetLegendHint">{{ fieldsetLegendHint
-                  }}</span>
+              <legend class="fr-fieldset__legend fr-fieldset__legend--regular">{{ fieldsetLegend }}
+                <span class="fr-hint-text" v-show="fieldsetLegendHint">{{ fieldsetLegendHint }}</span>
               </legend>
-              <div class="fr-fieldset__element" v-for="(option, index) in filteredOptions" :key="option.value">
+              <div class="fr-fieldset__element" v-for="option in filteredOptions" :key="option.value">
                 <div class="fr-checkbox-group">
                   <input type="checkbox" :id="option.value" :value="option.value" v-model="selectedOptions">
-                  <label class="fr-label" :for="option.value"> {{ option.label }} </label>
+                  <label class="fr-label" :for="option.value">{{ option.label }}</label>
                 </div>
               </div>
             </fieldset>
@@ -48,85 +49,122 @@
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, ref, computed, PropType, onMounted, onBeforeUnmount } from 'vue';
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+export default defineComponent({
+  name: 'DsfrSelectDropdown',
   props: {
     options: {
-      type: Array,
-      required: true
+      type: Array as PropType<Option[]>,
+      required: true,
     },
     label: {
       type: String,
-      required: true
+      required: true,
     },
     hint: {
-      type: String
+      type: String,
+      default: '',
     },
     fieldsetLegend: {
       type: String,
-      default: 'Choix de l\'option'
+      default: 'Choix de l\'option',
     },
     fieldsetLegendHint: {
-      type: String
+      type: String,
+      default: '',
     },
     showSelectAllBtn: {
-      type: Boolean
-    }
+      type: Boolean,
+      default: false,
+    },
   },
-  data() {
+  setup(props) {
+    const isDropdownOpen = ref(false);
+    const query = ref('');
+    const selectedOptions = ref<string[]>([]);
+    const selectButtonRef = ref<HTMLElement | null>(null);
+
+    const filteredOptions = computed(() => {
+      return props.options.filter(option =>
+        option.label.toLowerCase().includes(query.value.toLowerCase())
+      );
+    });
+
+    const selectedOptionsLabel = computed(() => {
+      if (selectedOptions.value.length === 1) return "Une option sélectionnée";
+      if (selectedOptions.value.length > 1) return `${selectedOptions.value.length} options sélectionnées`;
+      return "Sélectionner";
+    });
+
+    const dropdownId = `dropdown-${Math.random().toString(36).substr(2, 9)}`;
+
+    const allSelected = computed(() => {
+      return (
+        filteredOptions.value.length > 0 &&
+        filteredOptions.value.every(option => selectedOptions.value.includes(option.value))
+      );
+    });
+
+    const toggleDropdown = () => {
+      isDropdownOpen.value = !isDropdownOpen.value;
+    };
+
+    const selectAll = () => {
+      if (allSelected.value) {
+        selectedOptions.value = [];
+      } else {
+        selectedOptions.value = filteredOptions.value.map(option => option.value);
+      }
+    };
+
+    const removeTag = (tag: string) => {
+      const index = selectedOptions.value.indexOf(tag);
+      if (index !== -1) selectedOptions.value.splice(index, 1);
+    };
+
+    const getOptionLabel = (value: string) => {
+      const option = props.options.find(option => option.value === value);
+      return option ? option.label : '';
+    };
+
+    const handleKeydown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape' && isDropdownOpen.value) {
+        isDropdownOpen.value = false;
+        selectButtonRef.value?.focus();
+      }
+    };
+
+    onMounted(() => {
+      window.addEventListener('keydown', handleKeydown);
+    });
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('keydown', handleKeydown);
+    });
+
     return {
-      isDropdownOpen: false,
-      query: '',
-      selectedOptions: [],
+      isDropdownOpen,
+      query,
+      selectedOptions,
+      filteredOptions,
+      selectedOptionsLabel,
+      dropdownId,
+      allSelected,
+      toggleDropdown,
+      selectAll,
+      removeTag,
+      getOptionLabel,
+      selectButtonRef
     };
   },
-  computed: {
-    filteredOptions() {
-      return this.options.filter(option =>
-        option.label.toLowerCase().includes(this.query.toLowerCase())
-      );
-    },
-    selectedOptionsLabel() {
-      if (this.selectedOptions.length === 1) {
-        return "Une option sélectionnée";
-      } else if (this.selectedOptions.length > 1) {
-        return this.selectedOptions.length + " options sélectionnées";
-      } else {
-        return "Sélectionner";
-      }
-    },
-    dropdownId() {
-      return `dropdown-${Math.random().toString(36).substr(2, 9)}`
-    },
-    allSelected() {
-      return this.filteredOptions.length > 0 && this.filteredOptions.every(option =>
-        this.selectedOptions.includes(option.value)
-      );
-    }
-  },
-  methods: {
-    toggleDropdown() {
-      this.isDropdownOpen = !this.isDropdownOpen;
-    },
-    selectAll() {
-      if (this.allSelected) {
-        this.selectedOptions = [];
-      } else {
-        this.selectedOptions = this.filteredOptions.map(option => option.value);
-      }
-    },
-    removeTag(tag) {
-      const index = this.selectedOptions.indexOf(tag);
-      if (index !== -1) {
-        this.selectedOptions.splice(index, 1);
-      }
-    },
-    getOptionLabel(value) {
-      const option = this.options.find(option => option.value === value);
-      return option ? option.label : '';
-    }
-  },
-};
+});
 </script>
 
 <style scoped>
